f41b315ab9456021f0665935ce8dc4e11fe6e112
Wian Lloyd <wian.dev.inquire@gmail.com>
2020-06-06 02:12:06 +0200

    structure init
    · Added and configured Prettier to work with VScode, .editorconfig & tslint.json.
    · Added empty Firebase configuration.
    · Created core & assets structure.
    · Added FiraSans font TrueType files.

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// The values provided below are the defaults.
+// If you don't specify one of these properties, 
+// the default value will be applied.
+module.exports = {
+    printWidth: 80, 
+    tabWidth: 4,    
+    useTabs: true,  
+    semi: true,
+    singleQuote: false,
+    trailingComma: 'none', // other options `es5` or `all`
+    bracketSpacing: true,
+    arrowParens: 'always',  // other option 'avoid'
+    parser: 'typescript',
+};
+
+// https://prettier.io/docs/en/options.html
